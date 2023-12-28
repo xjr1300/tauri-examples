5368ca4688abf7827951d5218dfde9c2b5c053c7
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-12-28 13:18:48 +0900

    Implement a feature to save editor settings from frontend.

--- src/components/states/StateManagement.tsx
+++ src/components/states/StateManagement.tsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { useForm } from '@mantine/form';
 import { invoke } from '@tauri-apps/api';
 import {
+  Blockquote,
   Button,
   ButtonProps,
   Checkbox,
+  Divider,
   Flex,
   NumberInput,
   TextInput,
   createPolymorphicComponent,
 } from '@mantine/core';
 import styled from '@emotion/styled';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 const StateManagement: React.FC = () => {
   const form = useForm<EditorSettings>({
@@ -37,37 +40,49 @@ const StateManagement: React.FC = () => {
       form.setValues(editorSettings);
     })();
   }, []);
+  const icon = <IconInfoCircle />;
 
   return (
-    <form onSubmit={form.onSubmit(() => {})}>
-      <TextInput
-        withAsterisk
-        label="フォント"
-        placeholder="フォントを入力してください。"
-        {...form.getInputProps('fontFamily')}
-      />
-      <NumberInput
-        withAsterisk
-        label="タブサイズ"
-        placeholder="タブサイズを入力してください。"
-        {...form.getInputProps('tabSize')}
-      />
-      <Checkbox
-        label="タブをスペースで展開する"
-        mt="sm"
-        {...form.getInputProps('expandTab', { type: 'checkbox' })}
-      />
-      <Checkbox
-        label="テキストを端で折り返す"
-        mt="sm"
-        {...form.getInputProps('wordWrap', { type: 'checkbox' })}
-      />
-      <Flex justify="flex-end" mt="xs">
-        <StyledButton type="submit" variant="filled">
-          更新
-        </StyledButton>
-      </Flex>
-    </form>
+    <>
+      <Blockquote color="blue" radius="0" icon={icon} iconSize={30}>
+        設定はバックエンドで管理されています。更新ボタンを押して設定を更新すると、入力した設定がバックエンドに保存されます。他のコンポーネントを表示後、再度このコンポーネントを表示すると、設定が保存されていることを確認できます。
+      </Blockquote>
+      <Divider mt="sm" />
+      <form
+        onSubmit={form.onSubmit(async (values) => {
+          await invoke('save_editor_settings', { newSettings: values });
+        })}
+      >
+        <TextInput
+          withAsterisk
+          label="フォント"
+          placeholder="フォントを入力してください。"
+          mt="sm"
+          {...form.getInputProps('fontFamily')}
+        />
+        <NumberInput
+          withAsterisk
+          label="タブサイズ"
+          placeholder="タブサイズを入力してください。"
+          {...form.getInputProps('tabSize')}
+        />
+        <Checkbox
+          label="タブをスペースで展開する"
+          mt="sm"
+          {...form.getInputProps('expandTab', { type: 'checkbox' })}
+        />
+        <Checkbox
+          label="テキストを端で折り返す"
+          mt="sm"
+          {...form.getInputProps('wordWrap', { type: 'checkbox' })}
+        />
+        <Flex justify="flex-end" mt="xs">
+          <StyledButton type="submit" variant="filled">
+            更新
+          </StyledButton>
+        </Flex>
+      </form>
+    </>
   );
 };
 
